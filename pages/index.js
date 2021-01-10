b31eca6aa2623c3a20b71ab024a3466265d315ee
Gary Tou <gary@garytou.com>
2021-01-09 22:11:02 -0800

    more tweaks and link on home

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,28 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<h1 className={styles.title}>
-					Hello Professor <span>Brehm</span>!
+					Hello <span>PROG 109</span>!
 				</h1>
 
 				<p className={styles.description}>My name is Gary Tou</p>
+
+				<div className={styles.grid}>
+					<div className={styles.card}>
+						<h3>Module 01</h3>
+						<a href="/module01">
+							<p>Module Webpage &rarr;</p>
+						</a>
+						<a href="/module01/artifact01.html">
+							<p>Artifact 01 &rarr;</p>
+						</a>
+					</div>
+					<div className={styles.card}>
+						<h3>Module 02</h3>
+						<a href="/module02">
+							<p>Module Webpage &rarr;</p>
+						</a>
+					</div>
+				</div>
 			</main>
 
 			<footer className={styles.footer}>
@@ -24,6 +42,14 @@ export default function Home() {
 					rel="noopener noreferrer"
 				>
 					@garyhtou
+				</a>
+				<span className={styles.footerSep}> / </span>
+				<a
+					href="https://garytou.com/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					garytou.com
 				</a>
 			</footer>
 		</div>
